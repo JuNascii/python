2a82be6d0736f5f78ea33f7aac15ed2c91e1e346
JuNascii <123904795+JuNascii@users.noreply.github.com>
2023-01-30 03:59:15 -0300

    Add files via upload

---
+++ ex002.py
@@ -0,0 +1,4 @@
+print(f'Hello! Welcome to my first program written in Python!\nMy name is Julio (or JuNascii on GitHub)')
+name = input('Tell me your name: ')
+
+print(f"That's a great name, {name}! I hope you enjoy my evolution with programming in py.")
